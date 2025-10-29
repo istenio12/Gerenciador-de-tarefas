3903e13d9f7b16101e02da3f728f54296fe0e6f0
Istenio <istenionascimento38@gmail.com>
2025-10-29 08:52:29 -0300

    sincronizando projeto  com github

--- js/script.js
+++ js/script.js
@@ -10,13 +10,27 @@ document.getElementById('btnExportar').addEventListener('click', () => {
   let lista = JSON.parse(dados);
 
   // Monta a tabela HTML
-  let tabelaHTML = '<table><tr><th>Nome</th><th>Equipamento</th><th>Data</th></tr>';
+  let tabelaHTML = '<table border="1"><tr><th>Nome</th><th>Tarefa</th><th>Inicio</th><th>Prazo</th><th>Status</th></tr>';
   
 lista.forEach(obj => {
-    tabelaHTML += `<tr>
+    const hoje = new Date();
+    const inicio = new Date(obj.startDate);
+    const prazo = new Date(obj.endDate);
+    let status = '';
+
+    if(inicio) {
+      status = 'Nao iniciado';
+    }else if (hoje <= prazo){
+      status = 'Em andamento';
+    }else{
+      status = 'Atrasado';
+    }
+        tabelaHTML += `<tr>
       <td>${obj.person || ''}</td>
       <td>${obj.item || ''}</td>
-      <td>${obj.date || ''}</td>
+      <td>${obj.startDate || ''}</td>
+      <td>${obj.endDate || ''}</td>
+      <td>${status}</td>
     </tr>`;
   });
 
@@ -38,29 +52,54 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById('loanForm');
   const loanList = document.getElementById('loanList');
 
-  function loadLoans() {
-    const loans = JSON.parse(localStorage.getItem('loans')) || [];
-    loanList.innerHTML = '';
-    loans.forEach((loan, index) => {
-      const li = document.createElement('li');
-      li.innerHTML = `
-        <strong>${loan.item}</strong> para <em>${loan.person}</em> em ${loan.date}      
-      `;
+  
+function loadLoans() {
+  const loans = JSON.parse(localStorage.getItem('loans')) || [];
+  loanList.innerHTML = '';
+  loans.forEach((loan, index) => {
+    const hoje = new Date();
+    const inicio = new Date(loan.startDate);
+    const prazo = new Date(loan.endDate);
+
+    let statusTexto = '';
+    let statusClasse = '';
+
+    if (hoje < inicio) {
+      statusTexto = 'Nao iniciado';
+      statusClasse = 'status-verde';
+    } else if (hoje <= prazo) {
+      statusTexto = 'Em andamento';
+      statusClasse = 'status-verde';
+    } else {
+      statusTexto = 'Atrasado';
+      statusClasse = 'status-vermelho';
+    }
     
-const button = document.createElement("button");
-button.textContent = "Resolver";
-button.addEventListener("click", () => returnItem(index));
+    const li = document.createElement('li');
+
+      li.innerHTML = `
+        
+<strong>${loan.item}</strong> para <em>${loan.person}</em>
+      (Início: ${loan.startDate}, Prazo: ${loan.endDate})
+      <span class="status-btn
+      ${statusClasse}">${statusTexto}</span>
+    `;
+
+    const button = document.createElement("button");
+    button.textContent = "Resolver";
+    button.addEventListener("click", () => returnItem(index));
+
 li.appendChild(button)
    
       loanList.appendChild(li);
     });
   }
 
-  function saveLoan(item, person, date) {
+  function saveLoan(item, person, startDate, endDate) {
     const loans = JSON.parse(localStorage.getItem('loans')) || [];
-    loans.push({ item, person, date });
+    loans.push({ item, person, startDate, endDate });
     localStorage.setItem('loans', JSON.stringify(loans));
-    console.log("salvando:", item, person, date);
+    console.log("salvando:", item, person, startDate, endDate);
     loadLoans();
   }
 
@@ -76,8 +115,9 @@ li.appendChild(button)
     console.log("formulario enviado!");
     const item = document.getElementById('item').value;
     const person = document.getElementById('person').value;
-    const date = document.getElementById('date').value;
-    saveLoan(item, person, date);
+    const startDateDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
+    saveLoan(item, person, startDateDate, endDate);
     form.reset();
   });
 
